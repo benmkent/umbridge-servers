342dc6a3d08232938e015214f2a199fa6e495441
Ben Kent <benkent@live.co.uk>
2024-07-09 14:56:04 +0200

    fix detection of seperation. was spoilt by extrapolation

--- openfoam/postprocess_openfoam.py
+++ openfoam/postprocess_openfoam.py
@@ -6,7 +6,7 @@ def extract_reattachment_point(filename, final_time):
     Tx,Ty,Tz = fluidfoam.readfield(filename,name="wallShearStress",time_name=str(final_time),boundary="bottomWall")
     # Px,Py,Pz = fluidfoam.readfield(filename,name="p",time_name="5000",boundary="bottomWall")
 
-    spline = interpolate.CubicSpline(X,Tx)
+    spline = interpolate.CubicSpline(X,Tx, extrapolate=False)
 
     r = spline.roots()
 
