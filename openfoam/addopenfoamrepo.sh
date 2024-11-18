1b97b72e50d373da331a6810ebb18344d984a1a6
Ben Kent <benkent@live.co.uk>
2024-11-18 15:56:50 +0100

    try again

--- openfoam/addopenfoamrepo.sh
+++ openfoam/addopenfoamrepo.sh
@@ -139,7 +139,7 @@ then
     curl -L -k "${pubkey_url}" 2>/dev/null | gpg --dearmor > "$apt_pubkey_path"
     curl -k "${pubkey_url}" 2>/dev/null |
     gpg --dearmor |
-    sudo tee "$apt_pubkey_path" > /dev/null 
+    tee "$apt_pubkey_path" > /dev/null 
 else
     wget --no-check-certificate -O - -nv "${pubkey_url}" 2>/dev/null | gpg --dearmor > "$apt_pubkey_path"
 fi
